04cabef582aab859fc14aeba468600162e5ce552
Ismail Ali <esmailaly7400@gmail.com>
2022-06-04 07:15:37 +0200

    Updates

---
+++ precache-manifest.5bedb54c8f7f4f628143e6232f0b15b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97b0613dd581bafdb1fb",
+    "url": "/scandiweb-react-TEST/static/css/main.f23e39bd.chunk.css"
+  },
+  {
+    "revision": "97b0613dd581bafdb1fb",
+    "url": "/scandiweb-react-TEST/static/js/main.97b0613d.chunk.js"
+  },
+  {
+    "revision": "b8c2d59efc4ad3ba8664",
+    "url": "/scandiweb-react-TEST/static/js/1.b8c2d59e.chunk.js"
+  },
+  {
+    "revision": "8a4e9a265b4f1e34423b",
+    "url": "/scandiweb-react-TEST/static/js/runtime~main.8a4e9a26.js"
+  },
+  {
+    "revision": "510c38f0dcce2a8177a21d80827ada28",
+    "url": "/scandiweb-react-TEST/index.html"
+  }
+];
